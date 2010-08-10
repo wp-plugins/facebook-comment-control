e81276afd7e58374a27c4072ac51324a56530004
Unl3a5h3r <Unl3a5h3r@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-10 21:19:15 +0000

    git-svn-id: https://plugins.svn.wordpress.org/facebook-comment-control/tags/0.3@274126 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ check_options.php
@@ -0,0 +1,69 @@
+<?php
+	//check if there are already options set, otherwise: Default
+		//Check Layout
+		if(get_option('fbcomcon_layout')=='dark'){
+			$fbcomcon_layout_dark_sel = ' selected';
+		}
+		else{
+			$fbcomcon_layout_light_sel = ' selected';
+		}
+		//Check Userlevel
+		if(get_option('fbcomcon_userlevel')){
+			$fbcomcon_userlevel= get_option('fbcomcon_userlevel');
+			$fbcomcon_userlevel_sel = 'fbcomcon_userlevel_'.$fbcomcon_userlevel.'_sel';
+			$$fbcomcon_userlevel_sel = ' selected';
+		}
+		else{
+			$fbcomcon_userlevel = "10";
+		}
+		//Check Lang
+		if(get_option('fbcomcon_lang')){
+			$fbcomcon_lang = get_option('fbcomcon_lang');
+			$fbcomcon_lang_sel = 'fbcomcon_lang_'.$fbcomcon_lang.'_sel';
+			$$fbcomcon_lang_sel = ' selected';
+		}
+		else{
+			$fbcomcon_lang = "en_US";
+		}
+		//Check Comments Limit
+		if(get_option('fbcomcon_comments_limit')){
+			$comments_limit = get_option('fbcomcon_comments_limit');
+		}
+		else{
+			$comments_limit = '-1';
+		}
+		//Check Paging Limit
+		if(get_option('fbcomcon_paging_limit')){
+			$paging_limit = get_option('fbcomcon_paging_limit');
+		}
+		else{
+			$paging_limit = '10';
+		}
+		//Check Comments Paging
+		if(get_option('fbcomcon_paging_comments')=='false'){
+			$fbcomcon_paging_comments_no_sel = ' selected';
+			$paging_comments = 'false';
+		}
+		else{
+			$fbcomcon_paging_comments_yes_sel = ' selected';
+			$paging_comments = 'true';
+		}
+		//Check Comments Count
+		if(get_option('fbcomcon_comments_count')=='false'){
+			$fbcomcon_comments_count_no_sel = ' selected';
+			$comments_count = 'false';
+		}
+		else{
+			$fbcomcon_comments_count_yes_sel = ' selected';
+			$comments_count = 'true';
+		}
+		//Check Login Header
+		if(get_option('fbcomcon_login_header')=='false'){
+			$fbcomcon_login_header_no_sel = ' selected';
+			$login_header = 'false';
+		}
+		else{
+			$fbcomcon_login_header_yes_sel = ' selected';
+			$login_header = 'true';
+		}
+?>
